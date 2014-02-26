c67d075d0cd5616e25e15253778fc2a17cead2af
sbt-Ryabov-dv <sbt-Ryabov-dv@50I-130720127.sigma.sbrf.ru>
2014-02-26 18:21:01 +0400

    #1

--- src/vmdeep/chatlists/chat/Message.java
+++ src/vmdeep/chatlists/chat/Message.java
@@ -1,37 +1,37 @@
 package vmdeep.chatlists.chat;
 
 public class Message {
-	
-	String 	UserName;
-	String 	MessageText;
-	long 	timestamp;
-	public int setUserName(String str1) {
+
+	private String UserName;
+	private String MessageText;
+	private long timestamp;
+
+	public void setUserName(String str1) {
 		UserName = str1;
-		return 1;
 	};
+
 	public String getUserName() {
 		return UserName;
 	};
-	public int setMessageText(String str1) {
+
+	public void setMessageText(String str1) {
 		MessageText = str1;
-		return 1;
 	};
+
 	public String getMessageText() {
 		return MessageText;
-	};	
+	};
+
 	public long getMessageTimeStamp() {
-		java.util.Date timestamp= new java.util.Date();	
+		java.util.Date timestamp = new java.util.Date();
 		return timestamp.getTime();
-	};	
+	};
 
-	
-	
 	public static void main(String[] args) {
-			
-   // TODO Auto-generated method stub*/
-	//Message	MyMessage=new Message();
-	//MyMessage.UserName="Вася";
-    //System.out.println(MyMessage.getMessageTimeStamp());
-    };	
-}
 
+		// TODO Auto-generated method stub*/
+		// Message MyMessage=new Message();
+		// MyMessage.UserName="Вася";
+		// System.out.println(MyMessage.getMessageTimeStamp());
+	};
+}
