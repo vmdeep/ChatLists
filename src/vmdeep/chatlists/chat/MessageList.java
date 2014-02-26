391b7307ea269aa73fca2dfcaa1b552780670897
unlimit1984 <unlimit_rmc@mail.ru>
2014-02-26 14:45:59 +0400

    вовин коммит

--- src/vmdeep/chatlists/chat/MessageList.java
+++ src/vmdeep/chatlists/chat/MessageList.java
@@ -1,12 +1,49 @@
 package vmdeep.chatlists.chat;
 
-public class MessageList {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
-	/**
-	 * @param args
-	 */
+public class MessageList {
+	private int capacity=20;
+	private static int MIN_MESSAGE_COUNT=1;
+	private static int MAX_MESSAGE_COUNT=100;
+
+	private ConcurrentLinkedDeque<Message> messages=new ConcurrentLinkedDeque<Message>();
+	
+	{
+		for(int i=0;i<capacity;i++){
+			messages.add(null);
+		}
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		if(capacity>=MIN_MESSAGE_COUNT && capacity<=MAX_MESSAGE_COUNT)
+			this.capacity = capacity;
+	}
+	
+	
+	public void pushMessage(Message m){
+		messages.add(m);
+		while(messages.size()>capacity)
+			messages.removeFirst();
+	}
+	public Message getLastMessage(){
+		return messages.getLast();
+		
+	}
+//	public Message[] getAllMessage(){
+//		return messages.toArray
+//	}
+	
+
+
+
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
