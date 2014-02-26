cf79ac719fd9c9f96b27d7d67534783d64507738
sbt-Ryabov-dv <sbt-Ryabov-dv@50I-130720127.sigma.sbrf.ru>
2014-02-26 14:56:54 +0400

    коммит Димы

--- src/vmdeep/chatlists/chat/Message.java
+++ src/vmdeep/chatlists/chat/Message.java
@@ -1,13 +1,37 @@
 package vmdeep.chatlists.chat;
 
 public class Message {
+	
+	String 	UserName;
+	String 	MessageText;
+	long 	timestamp;
+	public int setUserName(String str1) {
+		UserName = str1;
+		return 1;
+	};
+	public String getUserName() {
+		return UserName;
+	};
+	public int setMessageText(String str1) {
+		MessageText = str1;
+		return 1;
+	};
+	public String getMessageText() {
+		return MessageText;
+	};	
+	public long getMessageTimeStamp() {
+		java.util.Date timestamp= new java.util.Date();	
+		return timestamp.getTime();
+	};	
 
-	/**
-	 * @param args
-	 */
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+			
+   // TODO Auto-generated method stub*/
+	//Message	MyMessage=new Message();
+	//MyMessage.UserName="Вася";
+    //System.out.println(MyMessage.getMessageTimeStamp());
+    };	
 }
+
