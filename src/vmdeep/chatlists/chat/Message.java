8c143dc91e15ede5c42c0037c7290393001e7052
Рябов Дима из дома <rdv.pvt@gmail.com>
2014-02-27 20:38:44 +0400

    Расширение класса Message member-ом

--- src/vmdeep/chatlists/chat/Message.java
+++ src/vmdeep/chatlists/chat/Message.java
@@ -2,12 +2,14 @@ package vmdeep.chatlists.chat;
 
 public class Message {
 
-	String MemberName;
-	String MessageText;
-	long timestamp;
+	private String MemberName;
+	private String MessageText;
+	private long timestamp;
+	private Member Member;   
 
-    public Message() //-------  Constructor
-    {
+    public Message(Member objMember) //-------  Constructor
+    {    
+    	Member = objMember;
     	timestamp =	(int) (System.currentTimeMillis() / 1000L);
     };
         
@@ -33,13 +35,19 @@ public class Message {
 
 		// java.util.Date timestamp= new java.util.Date();
 		// return timestamp.getTime();
-		return (int) (System.currentTimeMillis() / 1000L);
+		return timestamp;
 	};
 
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub*/
-		// Message MyMessage=new Message();
-		// System.out.println(MyMessage.timestamp);
+		 //Member MyMember = new Member();
+		 //MyMember.fullName="Рябов Дмитрий Викторович";
+		 //MyMember.email="rdv.pvt@gmail.com";
+		 //MyMember.nickName="Димон";
+		 //Message MyMessage=new Message(MyMember);
+		// System.out.println(MyMessage.Member.fullName);
+		 //System.out.println(MyMessage.timestamp);
+		 //System.out.println(MyMessage.getMessageTimeStamp());		 
 	};
 }
