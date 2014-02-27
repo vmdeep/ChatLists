927325fd7eca8065ceb7c8fe1daff8ea3e74bb99
Дима Р. из дома <rdv.pvt@gmail.com>
2014-02-27 19:55:18 +0400

    Добавлен конструктор класса Message

--- src/vmdeep/chatlists/chat/Message.java
+++ src/vmdeep/chatlists/chat/Message.java
@@ -1,40 +1,45 @@
 package vmdeep.chatlists.chat;
 
 public class Message {
-	
-	String 	UserName;
-	String 	MessageText;
-	long 	timestamp;
-	public int setUserName(String str1) {
-		UserName = str1;
+
+	String MemberName;
+	String MessageText;
+	long timestamp;
+
+    public Message() //-------  Constructor
+    {
+    	timestamp =	(int) (System.currentTimeMillis() / 1000L);
+    };
+        
+	public int setMemberName(String str1) {
+		MemberName = str1;
 		return 1;
 	};
-	public String getUserName() {
-		return UserName;
+
+	public String getMemberName() {
+		return MemberName;
 	};
+
 	public int setMessageText(String str1) {
 		MessageText = str1;
 		return 1;
 	};
+
 	public String getMessageText() {
 		return MessageText;
-	};	
+	};
+
 	public long getMessageTimeStamp() {
-		
-		//return (int) (System.currentTimeMillis() / 1000L); TIMESTAMP
-		
-		java.util.Date timestamp= new java.util.Date();
-		return timestamp.getTime();
-	};	
 
-	
-	
+		// java.util.Date timestamp= new java.util.Date();
+		// return timestamp.getTime();
+		return (int) (System.currentTimeMillis() / 1000L);
+	};
+
 	public static void main(String[] args) {
-			
-   // TODO Auto-generated method stub*/
-	//Message	MyMessage=new Message();
-	//MyMessage.UserName="Вася";
-    //System.out.println(MyMessage.getMessageTimeStamp());
-    };	
-}
 
+		// TODO Auto-generated method stub*/
+		// Message MyMessage=new Message();
+		// System.out.println(MyMessage.timestamp);
+	};
+}
