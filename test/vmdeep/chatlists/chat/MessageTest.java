f24c3198b92086c5a7fa37111a0b1e7cccda4389
Рябов Дима из дома <rdv.pvt@gmail.com>
2014-03-19 20:59:45 +0400

    Юнит-тесты для всех методов класса Message

--- test/vmdeep/chatlists/chat/MessageTest.java
+++ test/vmdeep/chatlists/chat/MessageTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 
 /**
  * @author Администратор
- *
+ * 
  */
 public class MessageTest {
 	String testEmail = "test@test.ru";
@@ -20,72 +20,66 @@ public class MessageTest {
 	String testFullname = "Вася Пупкин";
 	String testNickName = "}|{O|nOPA3PbIBATEJIb";
 
-//	MyMember.email=testEmail;
-//	MyMember.fullName=testFullname;
-//	MyMember.nickName=testNickName;
-	
-		
-	
-		
+	// MyMember.email=testEmail;
+	// MyMember.fullName=testFullname;
+	// MyMember.nickName=testNickName;
+
 	/**
 	 * Test method for {@link vmdeep.chatlists.chat.Message#Message()}.
 	 */
 	@Test
 	public void testMessage() {
-		Member MyMember = new Member();	
-		Message MyMessage = new Message(MyMember,testMessage);
-		assertEquals("Текст сообщения одинаковый",testMessage,MyMessage.getMessageText());
-		
+		Member MyMember = new Member();
+		Message MyMessage = new Message(MyMember, testMessage);
+		assertEquals("Текст сообщения одинаковый", testMessage,
+				MyMessage.getMessageText());
+
 	}
 
-	/**
-	 * Test method for {@link vmdeep.chatlists.chat.Message#Message(vmdeep.chatlists.chat.Member, java.lang.String)}.
-	 */
-	@Test
-	public void testMessageMemberString() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link vmdeep.chatlists.chat.Message#getMember()}.
-	 */
 	@Test
 	public void testGetMember() {
-		fail("Not yet implemented");
+		Member MyMember = new Member();
+		Message MyMessage = new Message(MyMember, testMessage);
+		assertEquals("Объект member один и тот же", MyMember,
+				MyMessage.getMember());
 	}
 
-	/**
-	 * Test method for {@link vmdeep.chatlists.chat.Message#setMember(vmdeep.chatlists.chat.Member)}.
-	 */
 	@Test
 	public void testSetMember() {
-		fail("Not yet implemented");
+		Member MyMember = new Member();
+		Member MyMember2 = new Member();
+		Message MyMessage = new Message(MyMember, testMessage);
+		MyMessage.setMember(MyMember2);
+		assertEquals("Объект member один и тот же", MyMember2,
+				MyMessage.getMember());
+
 	}
 
-	/**
-	 * Test method for {@link vmdeep.chatlists.chat.Message#setMessageText(java.lang.String)}.
-	 */
 	@Test
 	public void testSetMessageText() {
-		Message MyMessage2 = new Message();
-		MyMessage2.setMessageText("Hello");
-		assertEquals("Текст сообщения одинаковый","Hello",MyMessage2.getMessageText());
+		Member MyMember = new Member();		
+		Message MyMessage = new Message(MyMember, testMessage);
+		MyMessage.setMessageText("Hello");
+		assertEquals("Текст сообщения одинаковый", "Hello",
+				MyMessage.getMessageText());
 	}
 
-	/**
-	 * Test method for {@link vmdeep.chatlists.chat.Message#getMessageText()}.
-	 */
 	@Test
 	public void testGetMessageText() {
-		fail("Not yet implemented");
+		Member MyMember = new Member();		
+		Message MyMessage = new Message(MyMember, testMessage);
+		MyMessage.setMessageText("Hello");
+		assertEquals("Текст сообщения одинаковый", "Hello",
+				MyMessage.getMessageText());
 	}
 
-	/**
-	 * Test method for {@link vmdeep.chatlists.chat.Message#getMessageTimeStamp()}.
-	 */
 	@Test
 	public void testGetMessageTimeStamp() {
-		fail("Not yet implemented");
+		long timestamp;		
+		Member MyMember = new Member();
+		timestamp = System.currentTimeMillis() / 1000L;		
+		Message MyMessage = new Message(MyMember, testMessage);
+		assertEquals("Значения времени класса больше или равно", MyMessage.getMessageTimeStamp(), timestamp); //? подкоркой чувствую, что сдесь должно быть AssertFalse на сравнение больше и равно
 	}
 
 	/**
@@ -93,9 +87,11 @@ public class MessageTest {
 	 */
 	@Test
 	public void testIsNew() {
+		long timestamp;	
 		Member MyMember = new Member();
-		Message MyMessage = new Message(MyMember,testMessage);		
-		assertFalse(MyMessage.getMessageTimeStamp()<MyMessage.timestamp);  		
+		timestamp = System.currentTimeMillis() / 1000L;		
+		Message MyMessage = new Message(MyMember, testMessage);
+		assertFalse(MyMessage.getMessageTimeStamp() > timestamp);
 	}
 
 }
