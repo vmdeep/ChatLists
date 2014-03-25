df5ac99b70e44bce03dca0ce7da07f6be79076d7
Рябов Дима из дома <rdv.pvt@gmail.com>
2014-03-25 21:49:25 +0400

    1

--- src/vmdeep/chatlists/auth/User.java
+++ src/vmdeep/chatlists/auth/User.java
@@ -1,5 +1,42 @@
 package vmdeep.chatlists.auth;
 
-public class User {
+import java.util.Date;
 
+public class User {
+	String 	login;
+	String 	password;
+	String 	email;
+	Date 	birthDate;
+
+	boolean banned;
+	public User()
+	{
+				
+	};
+
+	public void Create()
+	{
+	//ToDo Create User in DB 	
+	};
+	
+	public void Delete()
+	{
+	//ToDo delete from DB 	
+	};
+	
+	public void Update()
+	{
+	//ToDo update profile in DB 	
+	};
+
+	public void SetPassword()
+	{//GeneratePassword
+		
+	};
+	
+	public void Block()
+	{
+		
+	}
+	
 }
