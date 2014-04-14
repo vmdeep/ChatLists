ab29505ab45c2f99f8a93d168aad0b0e1c092743
sbt-lichman-ak <sbt-lichman-ak@SBT-R9-P5N3D.sigma.sbrf.ru>
2014-04-14 17:47:12 +0400

    Signed-off-by: sbt-lichman-ak <sbt-lichman-ak@SBT-R9-P5N3D.sigma.sbrf.ru>

---
+++ src/vmdeep/chatlists/rest/RoomController.java
@@ -0,0 +1,12 @@
+package vmdeep.chatlists.rest;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import vmdeep.chatlists.chat.Room;
+
+public class RoomController {
+	private ConcurrentHashMap<String, Room> rooms = new ConcurrentHashMap<String, Room>();
+	
+	
+
+}
